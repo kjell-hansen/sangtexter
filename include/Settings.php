fba05f2d09184b050baac56638f70875ef5708ee
Kjell Hansen (S1-KJHA-9205) <kjell.hansen@gymnasium.ax>
2021-10-26 14:10:29 +0300

    Uppdaterat Settings.php

--- include/Settings.php
+++ include/Settings.php
@@ -1,25 +1,31 @@
 <?php
+
 declare (strict_types=1);
 
 // Inställningar för siten
 
-class Settings{
-    private $settingsfile=__DIR__ . "settings.json";
-    static function getSettings():\stdClass {
+class Settings {
+
+    private const settingsfile = __DIR__ . "/settings.json";
+
+    static function getSettings(): \stdClass {
         if (is_file(self::settingsfile)) {
-            $fileContent= file_get_contents(self::settingsfile);
-            $retur= json_decode($fileContent);
+            $fileContent = file_get_contents(self::settingsfile);
+            $retur = json_decode($fileContent);
         } else {
-            $retur=new stdClass;
+            $retur = new stdClass;
         }
-        
+
         return $retur;
     }
-    
+
     static function saveSettings(string $key, string $value) {
-        $content=self::getSettings();
-        $content->$key=$value;
-        $fileContent= json_encode($content, JSON_PRETTY_PRINT|JSON_PARTIAL_OUTPUT_ON_ERROR);
+        $content = self::getSettings();
+        $content->$key = $value;
+        $fileContent = json_encode($content, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_PARTIAL_OUTPUT_ON_ERROR);
         file_put_contents(self::settingsfile, $fileContent);
     }
+
 }
+
+// 'rotmapp' => 'C:/Users/kjellh/OneDrive - Ålands Gymnasium/Git/gitkurs/'
